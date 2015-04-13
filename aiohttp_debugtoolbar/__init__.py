e1312a1d3528a19250d575b112bfbc23553cb162
Nickolai Novik <is.infinity@yahoo.com>
2015-04-13 17:57:14 +0300

    fix import and tests

--- aiohttp_debugtoolbar/__init__.py
+++ aiohttp_debugtoolbar/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.0.1'
 
 from .middlewares import toolbar_middleware_factory
-from .main import setup
+from .main import setup, APP_KEY
 
-__all__ = ['setup', 'toolbar_middleware_factory']
+__all__ = ['setup', 'toolbar_middleware_factory', 'APP_KEY']
