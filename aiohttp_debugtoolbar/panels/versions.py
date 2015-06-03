55f4c6dd72187309b8428412c73b5256ab4517d0
Nickolai Novik <is.infinity@yahoo.com>
2015-06-03 20:30:32 +0300

    show package home pages on versions panel

--- aiohttp_debugtoolbar/panels/versions.py
+++ aiohttp_debugtoolbar/panels/versions.py
@@ -12,11 +12,22 @@ __all__ = ['VersionDebugPanel']
 packages = []
 for distribution in pkg_resources.working_set:
     name = distribution.project_name
-    deps = [d.project_name for d in distribution.requires()]
+    dependencies = [d.project_name for d in distribution.requires()]
+
+    # parse home page url
+    metadata_file = distribution.PKG_INFO
+    lines = distribution.get_metadata_lines(metadata_file)
+    url = '#'
+    for l in lines:
+        if l.startswith('Home-page:'):
+            url = l[10:].strip()
+            break
+
     packages.append({'version': distribution.version,
                      'lowername': name.lower(),
                      'name': name,
-                     'dependencies': deps})
+                     'dependencies': dependencies,
+                     'url': url})
 
 packages = sorted(packages, key=itemgetter('lowername'))
 aiohttp_version = pkg_resources.get_distribution('aiohttp').version
@@ -24,7 +35,7 @@ aiohttp_version = pkg_resources.get_distribution('aiohttp').version
 
 class VersionDebugPanel(DebugPanel):
     """
-    Panel that displays the Python version, the Pyramid version, and the
+    Panel that displays the Python version, the aiohttp version, and the
     versions of other software on your PYTHONPATH.
     """
     name = 'Version'
