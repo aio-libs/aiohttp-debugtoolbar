e8e90561534c61248445a756a4f768ad4cfcd7fb
Nickolai Novik <is.infinity@yahoo.com>
2015-09-05 21:51:47 +0300

    bump version to 0.0.4

--- aiohttp_debugtoolbar/__init__.py
+++ aiohttp_debugtoolbar/__init__.py
@@ -1,5 +1,5 @@
 from .middlewares import toolbar_middleware_factory, middleware
 from .main import setup, APP_KEY
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __all__ = ['setup', 'middleware', 'toolbar_middleware_factory', 'APP_KEY']
