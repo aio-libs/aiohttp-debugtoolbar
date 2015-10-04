b238c8161ef1bc3d20241b6dc7b2d5a10fce608a
Nickolai Novik <is.infinity@yahoo.com>
2015-10-04 20:15:47 +0300

    make middlewares tests to use fixtures

--- tests/test_middlware.py
+++ tests/test_middlware.py
@@ -1,237 +1,169 @@
 import asyncio
 import aiohttp
+import pytest
 import aiohttp_jinja2
-import jinja2
+import aiohttp_debugtoolbar
 from aiohttp import web
 
-from aiohttp_debugtoolbar import middleware, setup as tbsetup
 
-from .base import BaseTest
+@pytest.mark.run_loop
+def test_render_toolbar_page(loop, create_server):
+    @asyncio.coroutine
+    def handler(request):
+        return aiohttp_jinja2.render_template(
+            'tplt.html', request,
+            {'head': 'HEAD', 'text': 'text'})
+    app, url = yield from create_server()
+    app.router.add_route('GET', '/', handler)
+
+    # make sure that toolbar button present on apps page
+    # add cookie to enforce performance panel measure time
+    conn = aiohttp.TCPConnector(loop=loop, force_close=True)
+    cookie = {"pdtb_active": "pDebugPerformancePanel"}
+    resp = yield from aiohttp.request('GET', url+'/', cookies=cookie,
+                                      loop=loop, connector=conn)
+    assert 200 == resp.status
+    txt = yield from resp.text()
+    assert 'pDebugToolbarHandle' in txt
+    resp.close()
+
+    # make sure that debug toolbar page working
+    url = "{}/_debugtoolbar".format(url)
+    resp = yield from aiohttp.request('GET', url, loop=loop, connector=conn)
+    yield from resp.text()
+    assert 200 == resp.status
+    resp.close()
+    conn.close()
 
 
-class TestMiddleware(BaseTest):
+@pytest.mark.run_loop
+def test_render_with_exception(loop, create_server):
+    @asyncio.coroutine
+    def handler(request):
+        raise NotImplementedError
+
+    app, url = yield from create_server()
+    app.router.add_route('GET', '/', handler)
+    # make sure that exception page rendered
+    resp = yield from aiohttp.request('GET', url+'/', loop=loop)
+    txt = yield from resp.text()
+    assert 500 == resp.status
+    assert '<div class="debugger">' in txt
+
+
+@pytest.mark.run_loop
+def test_intercept_redirect(loop, create_server):
+    @asyncio.coroutine
+    def handler(request):
+        raise web.HTTPMovedPermanently(location='/')
+
+    app, url = yield from create_server()
+    app.router.add_route('GET', '/', handler)
+    # make sure that exception page rendered
+    resp = yield from aiohttp.request(
+        'GET', url+'/', allow_redirects=False, loop=loop)
+    txt = yield from resp.text()
+    assert 200 == resp.status
+    assert 'Redirect intercepted' in txt
+
+
+@pytest.mark.run_loop
+def test_intercept_redirects_disabled(loop, create_server):
+    @asyncio.coroutine
+    def handler(request):
+        raise web.HTTPMovedPermanently(location='/')
+
+    app, url = yield from create_server(intercept_redirects=False)
+    app.router.add_route('GET', '/', handler)
+    # make sure that exception page rendered
+    resp = yield from aiohttp.request(
+        'GET', url+'/', allow_redirects=False, loop=loop)
+    txt = yield from resp.text()
+    assert 301 == resp.status
+    assert '301: Moved Permanently' == txt
+
+
+@pytest.mark.run_loop
+def test_toolbar_not_enabled(loop, create_server):
+    @asyncio.coroutine
+    def handler(request):
+        return aiohttp_jinja2.render_template(
+            'tplt.html', request,
+            {'head': 'HEAD', 'text': 'text'})
+
+    app, url = yield from create_server(enabled=False)
+    app.router.add_route('GET', '/', handler)
+    # make sure that toolbar button NOT present on apps page
+    resp = yield from aiohttp.request('GET', url+'/', loop=loop)
+    assert 200 == resp.status
+    txt = yield from resp.text()
+    assert 'pDebugToolbarHandle' not in txt
+
+    # make sure that debug toolbar page working
+    url = "{}/_debugtoolbar".format(url)
+    resp = yield from aiohttp.request('GET', url, loop=loop)
+    yield from resp.text()
+    assert 200 == resp.status
+
+
+@pytest.mark.run_loop
+def test_toolbar_content_type_json(loop, create_server):
 
     @asyncio.coroutine
-    def _setup_app(self, handler, **kw):
-        app = web.Application(loop=self.loop,
-                              middlewares=[middleware])
+    def handler(request):
+        response = web.Response(status=200)
+        response.content_type = 'application/json'
+        response.text = '{"a": 42}'
+        return response
 
-        tbsetup(app, **kw)
+    app, url = yield from create_server()
+    app.router.add_route('GET', '/', handler)
+    # make sure that toolbar button NOT present on apps page
+    resp = yield from aiohttp.request('GET', url+'/', loop=loop)
+    payload = yield from resp.json()
+    assert 200 == resp.status
+    assert payload == {"a": 42}
 
-        tplt = "<html><body><h1>{{ head }}</h1>{{ text }}</body></html>"
-        loader = jinja2.DictLoader({'tplt.html': tplt})
-        aiohttp_jinja2.setup(app, loader=loader)
 
-        app.router.add_route('GET', '/', handler)
+@pytest.mark.run_loop
+def test_do_not_intercept_exceptions(loop, create_server):
 
-        handler = app.make_handler()
-        srv = yield from self.loop.create_server(
-            handler, '127.0.0.1', self.port)
-        return app, srv, handler
+    @asyncio.coroutine
+    def handler(request):
+        raise NotImplementedError
 
-    def test_render_toolbar_page(self):
-        @asyncio.coroutine
-        def func(request):
-            return aiohttp_jinja2.render_template(
-                'tplt.html', request,
-                {'head': 'HEAD', 'text': 'text'})
+    app, url = yield from create_server(intercept_exc=False)
+    app.router.add_route('GET', '/', handler)
+    # make sure that exception page rendered
+    resp = yield from aiohttp.request('GET', url+'/', loop=loop)
+    txt = yield from resp.text()
+    assert 500 == resp.status
+    assert '<div class="debugger">' not in txt
 
-        @asyncio.coroutine
-        def go():
-            app, srv, handler = yield from self._setup_app(func)
 
-            # make sure that toolbar button present on apps page
-            # add cookie to enforce performance panel measure time
-            conn = aiohttp.TCPConnector(loop=self.loop, force_close=True)
-            cookie = {"pdtb_active": "pDebugPerformancePanel"}
-            resp = yield from aiohttp.request('GET', self.url, cookies=cookie,
-                                              loop=self.loop, connector=conn)
-            self.assertEqual(200, resp.status)
-            txt = yield from resp.text()
-            self.assertTrue('pDebugToolbarHandle' in txt)
-            resp.close()
+@pytest.mark.run_loop
+def test_setup_not_called_exception(loop):
 
-            # make sure that debug toolbar page working
-            url = "{}/_debugtoolbar".format(self.url)
-            resp = yield from aiohttp.request('GET', url, loop=self.loop,
-                                              connector=conn)
-            yield from resp.text()
-            self.assertEqual(200, resp.status)
-            resp.close()
+    app = web.Application(loop=loop)
+    with pytest.raises(RuntimeError):
+        yield from aiohttp_debugtoolbar.middleware(app, lambda r: r)
 
-            conn.close()
 
-            yield from handler.finish_connections()
-            srv.close()
+@pytest.mark.run_loop
+def test_process_stream_response(loop, create_server):
+    @asyncio.coroutine
+    def handler(request):
+        response = web.StreamResponse(status=200)
+        response.content_type = 'text/html'
+        response.start(request)
+        response.write(b'text')
+        return response
 
-        self.loop.run_until_complete(go())
+    app, url = yield from create_server()
+    app.router.add_route('GET', '/', handler)
 
-    def test_render_with_exception(self):
-        @asyncio.coroutine
-        def func(request):
-            raise NotImplementedError
-
-        @asyncio.coroutine
-        def go():
-            app, srv, handler = yield from self._setup_app(func)
-            # make sure that exception page rendered
-            resp = yield from aiohttp.request('GET', self.url, loop=self.loop)
-            txt = yield from resp.text()
-            self.assertEqual(500, resp.status)
-            self.assertTrue('<div class="debugger">' in txt)
-
-            yield from handler.finish_connections()
-            srv.close()
-
-        self.loop.run_until_complete(go())
-
-    def test_intercept_redirect(self):
-        @asyncio.coroutine
-        def func(request):
-            raise web.HTTPMovedPermanently(location='/')
-
-        @asyncio.coroutine
-        def go():
-            app, srv, handler = yield from self._setup_app(func)
-
-            # make sure that exception page rendered
-            resp = yield from aiohttp.request(
-                'GET', self.url, allow_redirects=False, loop=self.loop)
-            txt = yield from resp.text()
-            self.assertEqual(200, resp.status)
-            self.assertTrue('Redirect intercepted' in txt)
-
-            yield from handler.finish_connections()
-            srv.close()
-
-        self.loop.run_until_complete(go())
-
-    def test_intercept_redirects_disabled(self):
-        @asyncio.coroutine
-        def func(request):
-            raise web.HTTPMovedPermanently(location='/')
-
-        @asyncio.coroutine
-        def go():
-            app, srv, handler = yield from self._setup_app(
-                func, intercept_redirects=False)
-
-            # make sure that exception page rendered
-            resp = yield from aiohttp.request(
-                'GET', self.url, allow_redirects=False, loop=self.loop)
-            txt = yield from resp.text()
-            self.assertEqual(301, resp.status)
-            self.assertEqual('301: Moved Permanently', txt)
-
-            yield from handler.finish_connections()
-            srv.close()
-
-        self.loop.run_until_complete(go())
-
-    def test_toolbar_not_enabled(self):
-        @asyncio.coroutine
-        def func(request):
-            return aiohttp_jinja2.render_template(
-                'tplt.html', request,
-                {'head': 'HEAD', 'text': 'text'})
-
-        @asyncio.coroutine
-        def go():
-            app, srv, handler = yield from self._setup_app(func, enabled=False)
-
-            # make sure that toolbar button NOT present on apps page
-            resp = yield from aiohttp.request('GET', self.url, loop=self.loop)
-            self.assertEqual(200, resp.status)
-            txt = yield from resp.text()
-            self.assertFalse('pDebugToolbarHandle' in txt)
-
-            # make sure that debug toolbar page working
-            url = "{}/_debugtoolbar".format(self.url)
-            resp = yield from aiohttp.request('GET', url, loop=self.loop)
-            yield from resp.text()
-            self.assertEqual(200, resp.status)
-
-            yield from handler.finish_connections()
-            srv.close()
-
-        self.loop.run_until_complete(go())
-
-    def test_toolbar_content_type_json(self):
-
-        @asyncio.coroutine
-        def func(request):
-            response = web.Response(status=200)
-            response.content_type = 'application/json'
-            response.text = '{"a": 42}'
-            return response
-
-        @asyncio.coroutine
-        def go():
-            app, srv, handler = yield from self._setup_app(func)
-
-            # make sure that toolbar button NOT present on apps page
-            resp = yield from aiohttp.request('GET', self.url, loop=self.loop)
-            payload = yield from resp.json()
-            self.assertEqual(200, resp.status)
-            self.assertEqual(payload, {"a": 42})
-
-            yield from handler.finish_connections()
-            srv.close()
-
-        self.loop.run_until_complete(go())
-
-    def test_do_not_intercept_exceptions(self):
-
-        @asyncio.coroutine
-        def func(request):
-            raise NotImplementedError
-
-        @asyncio.coroutine
-        def go():
-            app, srv, handler = yield from self._setup_app(
-                func, intercept_exc=False)
-            # make sure that exception page rendered
-            resp = yield from aiohttp.request('GET', self.url, loop=self.loop)
-            txt = yield from resp.text()
-            self.assertEqual(500, resp.status)
-            self.assertFalse('<div class="debugger">' in txt)
-
-            yield from handler.finish_connections()
-            srv.close()
-
-        self.loop.run_until_complete(go())
-
-    def test_setup_not_called_exception(self):
-
-        @asyncio.coroutine
-        def go():
-            app = web.Application(loop=self.loop)
-            with self.assertRaises(RuntimeError):
-                yield from middleware(app, lambda r: r)
-
-        self.loop.run_until_complete(go())
-
-    def test_process_stream_response(self):
-
-        @asyncio.coroutine
-        def func(request):
-            response = web.StreamResponse(status=200)
-            response.content_type = 'text/html'
-            response.start(request)
-            response.write(b'text')
-            return response
-
-        @asyncio.coroutine
-        def go():
-            app, srv, handler = yield from self._setup_app(func)
-
-            # make sure that toolbar button NOT present on apps page
-            resp = yield from aiohttp.request('GET', self.url, loop=self.loop)
-            payload = yield from resp.read()
-            self.assertEqual(200, resp.status)
-            self.assertEqual(payload, b'text')
-
-            yield from handler.finish_connections()
-            srv.close()
-
-        self.loop.run_until_complete(go())
+    # make sure that toolbar button NOT present on apps page
+    resp = yield from aiohttp.request('GET', url+'/', loop=loop)
+    payload = yield from resp.read()
+    assert 200 == resp.status
+    assert payload == b'text'
