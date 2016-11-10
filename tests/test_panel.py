94dac02149a09413cabbc33ca8158e34a8a44156
Alexander <ra@ecraft.ru>
2016-11-10 19:59:10 +0300

    Fix tests;

--- tests/test_panel.py
+++ tests/test_panel.py
@@ -6,35 +6,34 @@ from aiohttp_debugtoolbar.panels.base import DebugPanel
 from aiohttp import ClientSession
 
 
-@pytest.mark.run_loop
-def test_request_vars_panel(loop, create_server):
+@asyncio.coroutine
+def test_request_vars_panel(create_server, test_client):
     @asyncio.coroutine
     def handler(request):
         return aiohttp_jinja2.render_template(
             'tplt.html', request,
             {'head': 'HEAD', 'text': 'text'})
 
-    app, url = yield from create_server()
+    app = yield from create_server()
     app.router.add_route('GET', '/', handler)
     # add cookie to request
     cookie = {"aiodtb_cookie": "aioDebugRequestPanel_Cookie"}
-    resp = yield from ClientSession(loop=loop, cookies=cookie).request(
-        'GET', url + '/')
+    client = yield from test_client(app, cookies=cookie)
+    resp = yield from client.get('/')
     assert 200 == resp.status
     txt = yield from resp.text()
     # Toolbar Button exists on page
     assert 'pDebugToolbarHandle' in txt
 
     # make sure that debug toolbar page working
-    url = "{}/_debugtoolbar".format(url)
-    resp = yield from ClientSession(loop=loop).request('GET', url)
+    resp = yield from client.get('/_debugtoolbar')
     txt = yield from resp.text()
     assert 'aioDebugRequestPanel_Cookie' in txt
     assert 200 == resp.status
 
 
-@pytest.mark.run_loop
-def test_extra_panel(loop, create_server):
+@asyncio.coroutine
+def test_extra_panel(create_server, test_client):
     @asyncio.coroutine
     def handler(request):
         return aiohttp_jinja2.render_template(
@@ -43,7 +42,7 @@ def test_extra_panel(loop, create_server):
 
     class TestExtraPanel(DebugPanel):
         name = 'aioTestExtraPanel'
-        has_content = False
+        has_content = True
         template = 'test.jinja2'
         title = name
         nav_title = title
@@ -56,12 +55,13 @@ def test_extra_panel(loop, create_server):
             })
 
     parent_path = pathlib.Path(__file__).parent
-    app, url = yield from create_server(
+    app = yield from create_server(
         extra_panels=[TestExtraPanel],
         extra_templates=str(parent_path / 'tpl'))
     app.router.add_route('GET', '/', handler)
     # make sure that toolbar button present on apps page
-    resp = yield from ClientSession(loop=loop).request('GET', url + '/')
+    client = yield from test_client(app)
+    resp = yield from client.get('/')
     assert 200 == resp.status
     txt = yield from resp.text()
     assert 'pDebugToolbarHandle' in txt
@@ -70,8 +70,7 @@ def test_extra_panel(loop, create_server):
     assert 'test.jinja2' in app['aiohttp_debugtoolbar_jinja2'].list_templates()
 
     # make sure that debug toolbar page working and extra panel exists
-    url = "{}/_debugtoolbar".format(url)
-    resp = yield from ClientSession(loop=loop).request('GET', url)
+    resp = yield from client.get('/_debugtoolbar')
     txt = yield from resp.text()
     assert 200 == resp.status
     assert 'aioExtraPanelTemplate' in txt
