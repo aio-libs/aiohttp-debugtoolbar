bf3b1d64883afc38beb8986b4611909db4813353
Alexander <ra@ecraft.ru>
2016-11-22 22:59:10 +0300

    Make extra panels aiopg & aioredis relative to request context;

--- examples/extra_panels/extra_pgsql.py
+++ examples/extra_panels/extra_pgsql.py
@@ -9,89 +9,103 @@ from aiopg.cursor import Cursor
 __all__ = ['RequestPgDebugPanel']
 
 
+class RequestHandler(object):
+    def __init__(self):
+        self._queries = []
+        self._total_time = 0
+        # save original
+        self._tmp_execute = Cursor.execute
+
+    @property
+    def queries(self):
+        return self._queries
+
+    @property
+    def total_time(self):
+        return self._total_time
+
+    def _wrapper(self, func):
+        @functools.wraps(func)
+        @asyncio.coroutine
+        def wrapped(*args, **kwargs):
+            start = time.time()
+
+            if asyncio.iscoroutinefunction(func):
+                coro = func
+            else:
+                coro = asyncio.coroutine(func)
+            context = yield from coro(*args, **kwargs)
+
+            called_from = []
+            for stack in inspect.stack()[1:]:
+                called_from.append("/{0}:{1}".format(
+                    "/".join(stack[1].split('/')[-3:]), stack[2]))
+                if len(called_from) >= 2:
+                    break
+
+            elapsed = time.time() - start
+            arg = {
+                'query': args[1].strip().replace(
+                    "\n", "<br>").replace(
+                    "\t", "&nbsp;&nbsp;").replace(
+                    "    ", "&nbsp;&nbsp;"),
+                'params': args[2] if len(args) > 2 else [],
+                'other': dict(kwargs),
+                'elapsed': '%0.3f sec' % elapsed,
+                'called_from': "<br/>".join(reversed(called_from)),
+            }
+            self._queries.append(arg)
+            self._total_time += elapsed
+
+            return context
+
+        return wrapped
+
+    def on(self):
+        Cursor.execute = self._wrapper(Cursor.execute)
+
+    def off(self):
+        Cursor.execute = self._tmp_execute
+
+
 class RequestPgDebugPanel(DebugPanel):
     """
     A panel to display SQL queries.
     """
     name = 'PgSQL'
-    has_content = False
     template = 'request_pgsql.jinja2'
     title = 'PgSQL Queries'
     nav_title = title
+
+    def __init__(self, request):
+        super().__init__(request)
+        self._handler = RequestHandler()
+
+    @property
+    def has_content(self):
+        if self.data.get('queries'):
+            return True
+        return False
 
     @asyncio.coroutine
     def process_response(self, response):
         self.data = data = {}
         data.update({
             'timing_rows': {
-                'Total time': '%0.3f sec' % self.total_time,
-                'Total': len(self._queries),
+                'Total time': '%0.3f sec' % self._handler.total_time,
+                'Total': len(self._handler.queries),
             }.items(),
-            'queries': [(k, v) for k, v in enumerate(self._queries)],
+            'queries': [(k, v)
+                        for k, v in enumerate(self._handler.queries)],
         })
-        if self._queries:
-            self.has_content = True
 
-    def _wrap_handler(self, handler):
+    def _install_handler(self):
+        self._handler.on()
 
-        def wrapper(func):
-            @functools.wraps(func)
-            @asyncio.coroutine
-            def wrapped(*args, **kwargs):
-                start = time.time()
-
-                if asyncio.iscoroutinefunction(func):
-                    coro = func
-                else:
-                    coro = asyncio.coroutine(func)
-                context = yield from coro(*args, **kwargs)
-
-                called_from = []
-                for stack in inspect.stack()[1:]:
-                    called_from.append("/{0}:{1}".format(
-                        "/".join(stack[1].split('/')[-3:]), stack[2]))
-                    if len(called_from) >= 2:
-                        break
-
-                elapsed = time.time() - start
-                arg = {
-                    'query': args[1].strip().replace(
-                        "\n", "<br>").replace(
-                        "\t", "&nbsp;&nbsp;").replace(
-                        "    ", "&nbsp;&nbsp;"),
-                    'params': args[2] if len(args) > 2 else [],
-                    'other': dict(kwargs),
-                    'elapsed': '%0.3f sec' % elapsed,
-                    'called_from': "<br/>".join(reversed(called_from)),
-                }
-                self._queries.append(arg)
-                self.total_time += elapsed
-
-                return context
-
-            return wrapped
-
-        @asyncio.coroutine
-        def timer_handler(request):
-            # save original
-            tmp_query = Cursor.execute
-            Cursor.execute = wrapper(Cursor.execute)
-
-            self._queries = []
-            self.total_time = 0
-
-            try:
-                result = yield from handler(request)
-            except:
-                raise
-            finally:
-                # restore original
-                Cursor.execute = tmp_query
-
-            return result
-
-        return timer_handler
+    def _uninstall_handler(self):
+        self._handler.off()
 
     def wrap_handler(self, handler, context_switcher):
-        handler = self._wrap_handler(handler)
+        context_switcher.add_context_in(self._install_handler)
+        context_switcher.add_context_out(self._uninstall_handler)
         return handler
--- examples/extra_panels/extra_redis.py
+++ examples/extra_panels/extra_redis.py
@@ -9,88 +9,101 @@ from aioredis import RedisConnection
 __all__ = ['RequestRedisDebugPanel']
 
 
+class RequestHandler(object):
+    def __init__(self):
+        self._queries = []
+        self._total_time = 0
+        # save original
+        self._tmp_execute = RedisConnection.execute
+
+    @property
+    def queries(self):
+        return self._queries
+
+    @property
+    def total_time(self):
+        return self._total_time
+
+    def _wrapper(self, func):
+        @functools.wraps(func)
+        @asyncio.coroutine
+        def wrapped(*args, **kwargs):
+            start = time.time()
+
+            if asyncio.iscoroutinefunction(func):
+                coro = func
+            else:
+                coro = asyncio.coroutine(func)
+            context = yield from coro(*args, **kwargs)
+
+            called_from = []
+            for stack in inspect.stack()[1:]:
+                called_from.append("/{0}:{1}".format(
+                    "/".join(stack[1].split('/')[-3:]), stack[2]))
+                if len(called_from) >= 2:
+                    break
+
+            elapsed = time.time() - start
+            arg = {
+                'command': (args[1].decode("UTF-8").strip()
+                            if isinstance(args[1], bytes) else args[1]),
+                'return': bool(context),
+                'key': args[2].strip(),
+                'params': {**{'args': args[4:]}, **dict(kwargs)},
+                'elapsed': '%0.3f sec' % elapsed,
+                'called_from': "<br/>".join(reversed(called_from)),
+            }
+            self._queries.append(arg)
+            self._total_time += elapsed
+
+            return context
+
+        return wrapped
+
+    def on(self):
+        RedisConnection.execute = self._wrapper(RedisConnection.execute)
+
+    def off(self):
+        RedisConnection.execute = self._tmp_execute
+
+
 class RequestRedisDebugPanel(DebugPanel):
     """
     A panel to display cache requests.
     """
     name = 'Redis'
-    has_content = False
     template = 'request_redis.jinja2'
     title = 'Redis'
     nav_title = title
+
+    def __init__(self, request):
+        super().__init__(request)
+        self._handler = RequestHandler()
+
+    @property
+    def has_content(self):
+        if self.data.get('queries'):
+            return True
+        return False
 
     @asyncio.coroutine
     def process_response(self, response):
         self.data = data = {}
         data.update({
             'timing_rows': {
-                'Total time': '%0.3f sec' % self.total_time,
-                'Total': len(self._queries),
-                # 'Active': Cache().is_active,
+                'Total time': '%0.3f sec' % self._handler.total_time,
+                'Total': len(self._handler.queries),
             }.items(),
-            'queries': [(k, v) for k, v in enumerate(self._queries)],
+            'queries': [(k, v) for k, v in enumerate(self._handler.queries)],
         })
-        if self._queries:
-            self.has_content = True
 
-    def _wrap_handler(self, handler):
+    def _install_handler(self):
+        self._handler.on()
 
-        def wrapper(func):
-            @functools.wraps(func)
-            @asyncio.coroutine
-            def wrapped(*args, **kwargs):
-                start = time.time()
-
-                if asyncio.iscoroutinefunction(func):
-                    coro = func
-                else:
-                    coro = asyncio.coroutine(func)
-                context = yield from coro(*args, **kwargs)
-
-                called_from = []
-                for stack in inspect.stack()[1:]:
-                    called_from.append("/{0}:{1}".format(
-                        "/".join(stack[1].split('/')[-3:]), stack[2]))
-                    if len(called_from) >= 2:
-                        break
-
-                elapsed = time.time() - start
-                arg = {
-                    'command': args[1].decode("UTF-8").strip(),
-                    'return': bool(context),
-                    'key': args[2].strip(),
-                    'params': {**{'args': args[4:]}, **dict(kwargs)},
-                    'elapsed': '%0.3f sec' % elapsed,
-                    'called_from': "<br/>".join(reversed(called_from)),
-                }
-                self._queries.append(arg)
-                self.total_time += elapsed
-
-                return context
-
-            return wrapped
-
-        @asyncio.coroutine
-        def timer_handler(request):
-            # save original
-            tmp = RedisConnection.execute
-            RedisConnection.execute = wrapper(RedisConnection.execute)
-
-            self._queries = []
-            self.total_time = 0
-
-            try:
-                result = yield from handler(request)
-            except:
-                raise
-            finally:
-                # restore original
-                RedisConnection.execute = tmp
-
-            return result
-
-        return timer_handler
+    def _uninstall_handler(self):
+        self._handler.off()
 
     def wrap_handler(self, handler, context_switcher):
-        handler = self._wrap_handler(handler)
+        context_switcher.add_context_in(self._install_handler)
+        context_switcher.add_context_out(self._uninstall_handler)
         return handler
