169e443790dcb61450f53db869a67ba170a8598f
Nickolai Novik <nickolainovik@gmail.com>
2017-05-04 23:18:48 +0300

    update version and changes

--- aiohttp_debugtoolbar/__init__.py
+++ aiohttp_debugtoolbar/__init__.py
@@ -1,6 +1,6 @@
 from .middlewares import toolbar_middleware_factory, middleware
 from .main import setup, APP_KEY
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 __all__ = ['setup', 'middleware', 'toolbar_middleware_factory', 'APP_KEY']
