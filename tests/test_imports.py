b3edf993d3950141a89766b8392395cdc7335fed
Sam Bull <git@sambull.org>
2022-02-06 20:34:40 +0000

    Fix

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -12,7 +12,7 @@ def test_import_time(pytester: pytest.Pytester) -> None:
     added that significantly increases import time.
     """
     r = pytester.run(
-        sys.executable, "-We", "-c", "import aiohttp_debugtoolbar", timeout=0.8
+        sys.executable, "-We", "-c", "import aiohttp_debugtoolbar", timeout=0.9
     )
 
     assert not r.stdout.str()
