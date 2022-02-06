92ab58d529260dbf3ae7d53be78d59ae8913cb63
Sam Bull <git@sambull.org>
2022-02-06 20:31:48 +0000

    Skip Pypy

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -4,7 +4,7 @@ import sys
 import pytest
 
 
-@pytest.skipif(platform.python_implementation() == "PyPy", reason="Too slow")
+@pytest.mark.skipif(platform.python_implementation() == "PyPy", reason="Too slow")
 def test_import_time(pytester: pytest.Pytester) -> None:
     """Check that importing aiohttp-debugtoolbar doesn't take too long.
     Obviously, the time may vary on different machines and may need to be adjusted
