478914b5c2e1e6a5f058e9b6ca8455bc8a07fbde
Sam Bull <git@sambull.org>
2023-09-13 19:42:51 +0100

    Update test_panels_versions.py

--- tests/test_panels_versions.py
+++ tests/test_panels_versions.py
@@ -14,6 +14,6 @@ async def test_packages():
 
     jinja2_metadata = next(p for p in panel.data["packages"] if p["name"] == "Jinja2")
     assert "version" in jinja2_metadata
-    assert jinja2_metadata["lowercase"] == "jinja2"
+    assert jinja2_metadata["lowername"] == "jinja2"
     assert any("MarkupSafe" in d for d in jinja2_metadata["dependencies"])
     assert "url" in jinja2_metadata
