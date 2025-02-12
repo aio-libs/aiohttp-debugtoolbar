24fa148e70ae3bec891ea813eec6871480cd0dd4
Sam Bull <git@sambull.org>
2025-02-12 19:18:25 +0000

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -16,9 +16,8 @@ def test_import_time(pytester: pytest.Pytester) -> None:
     added that significantly increases import time.
     """
     best_time_ms = 1000
-    cmd = "import time, timeit; time.sleep(1); print(int(timeit.timeit('import aiohttp_debugtoolbar', number=1) * 1000))"
+    cmd = "import timeit; print(int(timeit.timeit('import aiohttp_debugtoolbar', number=1) * 1000))"
     for _ in range(3):
-        time.sleep(1)
         r = pytester.run(sys.executable, "-We", "-c", cmd)
 
         assert not r.stderr.str()
@@ -26,4 +25,4 @@ def test_import_time(pytester: pytest.Pytester) -> None:
         if runtime_ms < best_time_ms:
             best_time_ms = runtime_ms
 
-    assert best_time_ms < 200
+    assert best_time_ms < 300
