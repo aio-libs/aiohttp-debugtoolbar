60c24f8c55bac819b3c26dc38d0997113279daa3
Sam Bull <git@sambull.org>
2025-02-12 18:56:01 +0000

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import Path
+import sys
 
 from aiohttp_debugtoolbar.utils import addr_in, escape, format_fname
 
@@ -32,9 +32,10 @@ def test_module_file_path():
         "/usr/local/python/site-packages/",
     )
 
-    sys_path = map(lambda p: str(Path(p)), sys_path_l)
+    prefix = "c:" if sys.platform == "win32" else ""
+    sys_path = map(lambda p: prefix + p.replace("/", os.path.sep), sys_path_l)
     modpath = format_fname(
-        str(Path("/foo/bar/aiohttp_debugtoolbar/tests/debugfoo.py")),
+        prefix + "/foo/bar/aiohttp_debugtoolbar/tests/debugfoo.py".replace("/", os.path.sep),
         sys_path,
     )
     expected = "<aiohttp_debugtoolbar/tests/debugfoo.py>".replace("/", os.path.sep)
@@ -42,8 +43,8 @@ def test_module_file_path():
 
 
 def test_no_matching_sys_path():
-    val = str(Path("/foo/bar/aiohttp_debugtoolbar/foo.py"))
-    sys_path = [str(Path("/bar/baz"))]
+    val = "/foo/bar/aiohttp_debugtoolbar/foo.py"
+    sys_path = [("c:" if sys.platform == "win32" else "") + "/bar/baz"]
     expected = "</foo/bar/aiohttp_debugtoolbar/foo.py>".replace("/", os.path.sep)
     assert format_fname(val, sys_path) == expected
 
